0d50facdca46877aa7d3cff5deb1b8513b1afabb
Alexing-uni <uo296505@uniovi.es>
2026-04-27 01:22:27 +0200

    Lab2: modelos QAK luciernas y comparacion Javalin vs QAK

--- qakdemo26/src/it/unibo/sender/Sender.kt
+++ qakdemo26/src/it/unibo/sender/Sender.kt
@@ -29,23 +29,38 @@ class Sender ( name: String, scope: CoroutineScope, isconfined: Boolean=false, i
 	override fun getBody() : (ActorBasicFsm.() -> Unit){
 		//val interruptedStateTransitions = mutableListOf<Transition>()
 		//IF actor.withobj !== null val actor.withobj.name� = actor.withobj.method�ENDIF
+		 var emitEvents = true   
+			   var DT         = 1000L; 
 		return { //this:ActionBasciFsm
 				state("s0") { //this:State
 					action { //it:State
-						CommUtils.outblack("Sender   | Iniciado. Preparando envío...")
-						delay(1000) 
-						CommUtils.outblack("Sender   | Enviando mensaje al receiver...")
-						forward("saludo", "saludo(Hola_desde_el_Sender)" ,"receiver" ) 
+						 logger.info(  "${currentState.stateName} handling $currentMsg"  )  
+						CommUtils.outgreen("$name sends msg1: msg1(1)... ")
+						 logger.info(  "${currentState.stateName} sending msg1:msge1(1)"  )  
+						forward("msg1", "msg1(1)" ,"receiver" ) 
+						delay(300) 
+						CommUtils.outgreen("$name sends msg1: msg1(2)... ")
+						 logger.info(  "${currentState.stateName} sending msg1:msge1(2)"  )  
+						forward("msg1", "msg1(2)" ,"receiver" ) 
 						//genTimer( actor, state )
 					}
 					//After Lenzi Aug2002
 					sysaction { //it:State
+				 	 		stateTimer = TimerActor("timer_s0", 
+				 	 					  scope, context!!, "local_tout_"+name+"_s0", DT )  //OCT2023
 					}	 	 
-					 transition( edgeName="goto",targetState="fin", cond=doswitch() )
+					 transition(edgeName="t03",targetState="sendothermsgs",cond=whenTimeout("local_tout_"+name+"_s0"))   
 				}	 
-				state("fin") { //this:State
+				state("sendothermsgs") { //this:State
 					action { //it:State
-						CommUtils.outblack("Sender   | He terminado mi trabajo.")
+						 logger.info(  "${currentState.stateName} handling $currentMsg"  )  
+						CommUtils.outgreen("$name sends msg2 after $DT ... ")
+						 logger.info(  "${currentState.stateName} sending msg2:msg2(1)"  )  
+						forward("msg2", "msg2(1)" ,"receiver" ) 
+						if(  emitEvents  
+						 ){ logger.info(  "${currentState.stateName} emits alarm"  )  
+						emit("alarm", "alarm(fire)" ) 
+						}
 						//genTimer( actor, state )
 					}
 					//After Lenzi Aug2002
